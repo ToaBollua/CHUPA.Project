9777d8b6190278639b21a71840f949a82fd28198
ToaBollua <Bollua@proton.me>
2023-09-01 12:32:41 -0400

    [RECHAZADA] Atributo de personas subdivididos

---
+++ 230901_ObjetoFletes[REDACTED].py
@@ -0,0 +1,50 @@
+class cliente:
+    id = 0  
+    nombre = ""
+    apellido_paterno = ""
+    apellido_materno = ""
+    rut = ""
+    fecha_nacimiento = ""
+    genero = ""
+    cargo = ""
+    direccion = ""
+    numero_de_contacto = 0
+    correo_electrónico = ""
+    factura = 0
+    fecha_pedido = ""
+    fecha_envio = ""
+    fecha_entrega = ""
+
+class cliente_empresa:
+    id = 0  
+    nombre = ""
+    apellido_paterno = ""
+    apellido_materno = ""
+    rut = ""
+    fecha_nacimiento = ""
+    giro = ""
+    razon_social = ""
+    direccion = ""
+    numero_de_contacto = 0
+    correo_electrónico = ""
+    factura = 0
+    fecha_pedido = ""
+    fecha_envio = ""
+    fecha_entrega = ""
+
+class trabajadores:
+    id = 0  
+    nombre_contacto =""
+    telefono_contacto = ""
+    rut = ""
+    fecha_nacimiento:""
+    genero = ""
+    cargo = ""
+    direcion = ""
+    tarifa = ""
+    numero_de_contacto = 0
+    correo_electrónico = ""
+    fecha_de_incorporación = ""
+    remuneración = 0
+
+    
